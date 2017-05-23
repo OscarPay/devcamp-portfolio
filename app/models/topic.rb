27f24c02370c5bcfbb4774ad718cd4b4e3c6d2e8
Oscar <nike-o_94@hotmail.com>
2017-05-23 16:44:25 -0500

    Implemented the blod widget for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,10 @@ class Topic < ApplicationRecord
   validates_presence_of :title
 
   has_many :blogs
+
+  def self.with_blogs
+    # Muestra solo los que tienen pero el GUEST User no puede ver los drafts así que el topic puede tener blogs
+    # que solo tienen draft y para el guestuser sera vacío
+    includes(:blogs).where.not(blogs: {id: nil})
+  end
 end
